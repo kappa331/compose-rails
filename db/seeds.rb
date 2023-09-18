3ad60c8ddd50a7a34a436dfc371ff4f960af4b06
root <wawata1qwerty@gmail.com>
2023-09-18 15:25:54 +0000

    add delete process

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Category.create(:name => "牛骨")
+Category.create(:name => "豚骨")
+Category.create(:name => "味噌")
+Category.create(:name => "醤油")
+Category.create(:name => "塩")
+
+Shop.create(:category_id => 2, :name => "一杉", :address=> "山口県宇部市厚南北1-3-23 グランフォート際波 104")
+Shop.create(:category_id => 3, :name => "萬馬軒", :address=> "山口県宇部市東須恵138-1")
+Shop.create(:category_id => 4, :name => "鶏そば すけ兵衛", :address=> "山口県宇部市松島町19-1")
